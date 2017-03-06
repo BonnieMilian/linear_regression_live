cc1dd2785b6a3a4294911e513e0d8bcef2813744
BonnieMilian <bonniemilian@galileo.edu>
2017-03-06 12:15:38 -0600

    Linear Regression Steps, graph 1000 train, FRM 1 to 8, graphs generated

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # linear_regression_live
-This is the code for the "How to Do Linear Regression the Right Way" live session by Siraj Raval on Youtube
+This code is a modification of the "How to Do Linear Regression the Right Way" live session by Siraj Raval on Youtube
 
 
 ##Overview
 
-This is the code for [this](https://youtu.be/uwwWVAgJBcM) video on Youtube by Siraj Raval. I'm using a small dataset of student test scores and the amount of hours they studied. Intuitively, there must be a relationship right? The more you study, the better your test scores should be. We're going to use [linear regression](https://onlinecourses.science.psu.edu/stat501/node/250) to prove this relationship. 
+Thanks a lot Siraj Raval :D
+
+Original code was for [this](https://youtu.be/uwwWVAgJBcM) video on Youtube by Siraj Raval.
+
+Siraj Raval: I'm using a small dataset of student test scores and the amount of hours they studied. Intuitively, there must be a relationship right? The more you study, the better your test scores should be. We're going to use [linear regression](https://onlinecourses.science.psu.edu/stat501/node/250) to prove this relationship. 
+
+
+Me: Added functions to visualize Linear Regression by Steps, graphs are generated
 
 Here are some helpful links:
 
@@ -23,10 +30,16 @@ https://spin.atomicobject.com/wp-content/uploads/linear_regression_gradient1.png
 
 Python 2 and 3 both work for this. Use [pip](https://pip.pypa.io/en/stable/) to install any dependencies.
 
+If you use Anaconda added an enviorment 'enviorment_siraj-regression.yaml'
+
 ##Usage
 
 Just run ``demo.py`` to see the results.
 
+``python demo.py`` on terminal.
+
 ##Credits
 
-Credits for this code go to [mattnedrich](https://github.com/mattnedrich). I've merely created a wrapper to get people started. 
+Credits for this code go to [mattnedrich](https://github.com/mattnedrich).
+
+Siraj Raval master branch [linear_regression_live](https://github.com/llSourcell/linear_regression_live).
--- demo.py
+++ demo.py
@@ -1,44 +1,95 @@
 from numpy import *
+import numpy as np
 # y = mx + b
 # m is slope, b is y-intercept
 def compute_error_for_line_given_points(b, m, points):
+    #initialize it at 0
     totalError = 0
+    #for every point
     for i in range(0, len(points)):
+        #get the x value
         x = points[i, 0]
+        #get the y value
         y = points[i, 1]
+        #get the difference, square it, add it to the total
         totalError += (y - (m * x + b)) ** 2
+    
+    #get the average
     return totalError / float(len(points))
 
 def step_gradient(b_current, m_current, points, learningRate):
+    #stating points for our gradients
     b_gradient = 0
     m_gradient = 0
+
     N = float(len(points))
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
         b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
         m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    
+    #update our b and m values using our partial  derivatives
     new_b = b_current - (learningRate * b_gradient)
     new_m = m_current - (learningRate * m_gradient)
     return [new_b, new_m]
 
 def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+     #starting b and m
     b = starting_b
     m = starting_m
+
+    #gradient descent
     for i in range(num_iterations):
+        #update b and m with the new more accurate b and m by performing
+        #this is gradient step
         b, m = step_gradient(b, m, array(points), learning_rate)
     return [b, m]
 
+def train(initial_b, initial_m, num_iterations, learning_rate, points):
+    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
+    return [b, m]
+
+def plot(bs, ms, points, colors):  
+    try:
+        import  matplotlib.pyplot as plt
+        plt.plot(points[:,0], points[:,1], 'ro', color='black')
+        
+        for i in range(0, len(colors)):
+            plt.plot(points[:,0], ms[i]*points[:,0] + bs[i], color=colors[i], linewidth=3)
+        plt.show()
+    except:
+        pass
+    return
+
 def run():
+    #Step 1 - collect our data
     points = genfromtxt("data.csv", delimiter=",")
+    
+    #Step 2 - define our hyperparameters
+    #how fast should our model converge?
     learning_rate = 0.0001
+    #y = mx + b
     initial_b = 0 # initial y-intercept guess
     initial_m = 0 # initial slope guess
-    num_iterations = 1000
+    num_iterations = 1 #how much train the model
+
+    b_s = array([]) #b in each iteration
+    m_s = array([]) #m in each iteration
+    colors = array(['crimson','red','orange','yellow','limegreen','green','teal','blue'])
+
+    #Step 3 - train our model
     print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points))
     print "Running..."
-    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
-    print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
-
+    
+    for i in range(0, len(colors)):
+        [b, m] = train(initial_b, initial_m, num_iterations, learning_rate, points)
+        b_s = np.append(b_s, [b])
+        m_s = np.append(m_s, [m])
+        print "After {0} iterations b = {1}, m = {2}, error = {3}, FRM {4}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points), i + 1)
+        num_iterations += 1
+    
+    plot(b_s, m_s, points, colors)
+    
 if __name__ == '__main__':
     run()
